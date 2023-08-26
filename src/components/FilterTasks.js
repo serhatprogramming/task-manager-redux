29c203d4055da2725bb554be31b66e0a28d98176
serhatprogramming <serhatprogramming@gmail.com>
2023-08-26 17:13:16 -0400

    initial setup no state yet.

--- src/components/FilterTasks.js
+++ src/components/FilterTasks.js
@@ -1,10 +1,7 @@
-import React from "react";
-
 const FilterTasks = () => {
   const handleFilterChange = (event) => {
     const selectedValue = event.target.value;
     console.log("Selected Filter:", selectedValue);
-    // Implement Redux action to update filter type
   };
 
   return (
