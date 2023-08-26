179c19ade3273a75780e1396bc26d809dc6be60b
serhatprogramming <serhatprogramming@gmail.com>
2023-08-26 18:34:00 -0400

    useSelector

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,14 +1,9 @@
+import { useSelector } from "react-redux";
 import Task from "./Task";
 import FilterTasks from "./FilterTasks";
 
 const TaskList = () => {
-  const tasks = [
-    { id: 1, description: "Finish the report", urgent: true },
-    { id: 2, description: "Buy groceries", urgent: false },
-    { id: 3, description: "Call John", urgent: false },
-    { id: 4, description: "Prepare presentation", urgent: true },
-  ];
-
+  const tasks = useSelector((state) => state.tasks);
   return (
     <div>
       <h2>Task List</h2>
